1ebe107937c0fa4b940ef8f31b02abfe2fd48411
iyosskoyos <iyoss.koyos@yahoo.com>
2017-11-15 17:12:28 +0700

    nambah course,header,sidebar
    sedikit update css

---
+++ IDE/layout/course.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>course</title>
+	<link rel="stylesheet" type="text/css" href="../lib/w3.css">
+	<link rel="stylesheet" href="../lib/w3-theme-dark-grey.css">
+	<link rel="stylesheet" href="../lib/font-awesome.min.css">
+	<link rel="stylesheet" href="../lib/font-awesome.css">
+	<link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+	<?php 
+	include('header.php');
+	include('sidebar.php');
+	?>
+</body>
+</html>
--- IDE/layout/header.php
+++ IDE/layout/header.php
@@ -1,20 +1,22 @@
-<div style="position:relative;width:100%; height:25%;" class="w3-theme-dark w3-text-white">
-	<div style="width:75%; float:left" class="w3-padding-left">
+<div style="position:relative;width:100%; height:20%; display: inline-block;" class="w3-theme-dark w3-text-white">
+	<div style="width:75%; float: left;" class="w3-padding-left">
+		<div style="margin: 10px">
 		<h1>Interactive Digital Learning Environtment</h1>
 		<h3>Faculty of information Technology and Science</h3>
 		<text >Parahyangan Catholic University</text>
-	
-	<hr style="display: block;
-    margin-top: 0.5em;
-    margin-bottom: 30px;
-    margin-left: 0px;
-    height:5px;
-background-color: white;">
+		</div>
+		
+		<hr style="display: block;
+		margin-top: 0.5em;
+		margin-bottom: 30px;
+		margin-left: 10px;
+		height:5px;
+		background-color: white;">
 	</div >
 
-	<div style="float:left;width:20%;margin-top:8px;margin-left:30px;"">
+	<div style="float:right;width:20%;margin-top:8px;margin-left:30px;"">
 
-	<img src="../img/logo.png"  width="160" height="160">
+		<img src="../img/logo.png"  width="160" height="160">
 
 	</div>
 </div>
