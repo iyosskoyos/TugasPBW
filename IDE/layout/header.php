d55116fa6f4f9c7022e16e73c571afee047549e6
iyosskoyos <iyoss.koyos@yahoo.com>
2017-11-15 11:54:17 +0700

    Input sql head and side bar

---
+++ IDE/layout/header.php
@@ -0,0 +1,20 @@
+<div style="position:relative;width:100%; height:25%;" class="w3-theme-dark w3-text-white">
+	<div style="width:75%; float:left" class="w3-padding-left">
+		<h1>Interactive Digital Learning Environtment</h1>
+		<h3>Faculty of information Technology and Science</h3>
+		<text >Parahyangan Catholic University</text>
+	
+	<hr style="display: block;
+    margin-top: 0.5em;
+    margin-bottom: 30px;
+    margin-left: 0px;
+    height:5px;
+background-color: white;">
+	</div >
+
+	<div style="float:left;width:20%;margin-top:8px;margin-left:30px;"">
+
+	<img src="../img/logo.png"  width="160" height="160">
+
+	</div>
+</div>
---
+++ IDE/layout/sidebar.php
@@ -0,0 +1,27 @@
+<div style="width:25%; height: 75%;position:absolute" class="w3-theme-l4 w3-padding-left">
+	<text style="margin-top:40px;display:block;">You are Logged in as</text>
+
+	<text  style="margin-top:10px;display:block;font-weight: bold; font-size:19px">2011730053</text>
+
+	<text  style="margin-top:10px;display:block;font-weight: bold; font-size:19px">Maria Veronica</text>
+
+	<hr style="display: block;
+    margin-top: 0.3em;
+    margin-bottom: 30px;
+    margin-left: 0px;
+    height:5px;
+    width:90%" class="w3-theme-l2">
+
+    <img class="w3-border " src="../img/profile.png"  width="130" height="130"><br>
+
+    <div class="w3-padding-left w3-margin-top">
+    	<a href="#" ><i  class = "fa fa-home " style="font-size:30px;margin-right:20px"></i></a><text style="font-size:20px;">HOME</text><br>
+
+    	<a href="#" ><i  class = "fa fa-list " style="font-size:28px;margin-right:20px ;margin-top:17px;"></i></a><text style="font-size:20px;">MY COURSES</text><br>
+    	
+    	<a href="#" ><i  class = "fa fa-user " style="font-size:30px;margin-right:20px ;margin-top:17px;"></i></a><text style="font-size:20px;">MY PROFILE</text><br>
+
+    	<a href="#" ><i  class = "fa fa-power-off " style="font-size:28px;margin-right:20px ;margin-top:17px;"></i></a><text style="font-size:20px;">LOG OUT</text><br>
+
+    </div>
+</div>
