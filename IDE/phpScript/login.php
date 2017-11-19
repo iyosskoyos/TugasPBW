3dd43a3dcfbdd79fc38799a1841e05da5ad18bfc
iyosskoyos <iyoss.koyos@yahoo.com>
2017-11-20 01:21:02 +0700

    add session BRAH

--- IDE/phpScript/login.php
+++ IDE/phpScript/login.php
@@ -1,5 +1,6 @@
 <?php 
 include 'connection.php';
+include 'startSession.php'
 ?>
 
 <?php
@@ -7,20 +8,35 @@ if (empty($_POST['username']) || empty($_POST['psw'])) {
 	header("Location: ../index.php");
 }
 else{
-	$username = $_POST['username'];
-	$password = $_POST['psw'];
+	$username = test_input($_POST['username']);
+	$password = test_input($_POST['psw']);
 	$sql = "SELECT username FROM users WHERE username = '$username'";
+	
+
 	$result = $mysqli->query($sql);
+
 	if($result && $result->num_rows > 0){
 		$sql = "SELECT username, pass FROM users WHERE username = '$username' AND pass= '$password'";
 		$result = $mysqli->query($sql);
+
 		if($result && $result->num_rows > 0){
-			$sql = "SELECT usergroups.name FROM users JOIN usergroups WHERE username = '$username' AND pass= '$password' ON users.ID_UG = usergroups.ID_UG";
-			$result = $mysqli->query($sql);
+			$stmt = "SELECT Users.ID_U as id, Users.username as username,
+			Users.pass as pass, Users.userID as userid, Users.name as name,
+			UserGroups.name as position FROM Users JOIN UserGroups ON Users.ID_UG = UserGroups.ID_UG 
+			WHERE Users.username = '$username'";
+			$result = $mysqli->query($stmt);
+
 			if($result && $result->num_rows > 0){
 				$row = $result->fetch_array();
-				$role = $row['name'];
-				if($role == 'student'){
+
+				$_SESSION['position'] = $row['position'];
+				$_SESSION['id'] = $row['id'];
+				$_SESSION['username'] = $row['username'];
+				$_SESSION['pass'] = $row['pass'];
+				$_SESSION['userid'] = $row['userid'];
+				$_SESSION['name'] = $row['name'];
+
+				if($_SESSION['position'] == 'student'){
 					header("Location: ../pages/student/std.php");
 				}
 				else{
@@ -35,6 +51,13 @@ else{
 	else{
 		echo "WRONG USERNAME";
 	}
+}
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 }
 
 ?>
